7d451f81454ccb40ba76c9d678254d01671c8083
Lasse Mortensen <lass8658@stud.kea.dk>
2019-10-15 12:01:38 +0200

    sæde og kvittering

---
+++ src/main/java/philip_lasse_christian_gustav_sofie/xpkino/Model/Saede.java
@@ -0,0 +1,22 @@
+package philip_lasse_christian_gustav_sofie.xpkino.Model;
+
+public class Saede {
+   private int Raekke;
+   private int Saedenummer;
+
+
+
+    public Saede(int raekke, int saedenummer){
+        Raekke = raekke;
+        Saedenummer = saedenummer;
+
+    }
+    public int getRaekke() { return Raekke; }
+
+    public void setRaekke(int raekke) { Raekke = raekke; }
+
+    public int getSaedenummer() { return Saedenummer; }
+
+    public void setSaedenummer(int saedenummer) { Saedenummer = saedenummer; }
+
+}
